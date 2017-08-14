c494b6313c8b2336c41af28a9169e03cf2163393
Azeem J <azeem@greenmediapartners.com>
2017-08-14 13:22:56 +0530

    validate author id

--- Api-laravel/app/lib/Authors.php
+++ Api-laravel/app/lib/Authors.php
@@ -1,4 +1,5 @@
-<?php
+<?php namespace App\lib;
+       use App\Author;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,6 +15,15 @@
 class Authors {
     //put your code here
     
-    
+    public static function getAuthorId($id) {
+
+        $author = Author::where('id', '=', $id)->first();
+        if ($author === null) {
+            // user doesn't exist
+            return -1;
+        }else{
+           return $author; 
+        }
+    }
     
 }
