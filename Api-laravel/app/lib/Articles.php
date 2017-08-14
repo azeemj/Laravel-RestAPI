6fc1fb98e482fe59da0e3a60da689ad63f2dfa29
Azeem J <azeem@greenmediapartners.com>
2017-08-14 14:26:42 +0530

    article update  with validation

--- Api-laravel/app/lib/Articles.php
+++ Api-laravel/app/lib/Articles.php
@@ -39,30 +39,75 @@ class Articles {
     }
 
     static function create($request) {
-        
-        try{
-            
-        
-        $article = new Article();
-        if (isset($request['title'])) {
-        $article->title = $request['title'];
-        }
-        
-        $article->author_id = $request['author_id'];
-        
-         
-          if (isset($request['url'])) {
-        $article->url = $request['url']; // return 1;
-         }
-          if (isset($request['content'])) {
-         $article->content = $request['content'];
-          }
-        
-        $article->save();
-        return $article->id;
-        } catch ( \Illuminate\Database\QueryException $e) {
-    
+
+        try {
+
+
+            $article = new Article();
+            if (isset($request['title'])) {
+                $article->title = $request['title'];
+            }
+
+            $article->author_id = $request['author_id'];
+
+
+            if (isset($request['url'])) {
+                $article->url = $request['url']; // return 1;
+            }
+            if (isset($request['content'])) {
+                $article->content = $request['content'];
+            }
+
+            $article->save();
+            return $article->id;
+        } catch (\Illuminate\Database\QueryException $e) {
+
             return $e;
+        }
+    }
+
+    public static function getArticleId($id) {
+
+        $article = Article::where('id', '=', $id)->first();
+        if ($article === null) {
+            // user doesn't exist
+            return -1;
+        } else {
+            return $article;
+        }
+    }
+
+    public static function updateArticle($request, $id) {
+
+        $article = \App\Article::find($id);
+
+        if (count($article) > 0) {
+
+
+            try {
+
+                if (isset($request['title'])) {
+                    $article->title = $request['title'];
+                }
+                if (isset($request['author_id'])) {
+                    $article->author_id = $request['author_id'];
+                }
+
+                if (isset($request['url'])) {
+                    $article->url = $request['url']; // return 1;
+                }
+                if (isset($request['content'])) {
+                    $article->content = $request['content'];
+                }
+
+                $article->save();
+                return $id;
+            } catch (\Illuminate\Database\QueryException $e) {
+
+                return $e;
+            }
+        } else {
+            return -1;
         }
     }
 
